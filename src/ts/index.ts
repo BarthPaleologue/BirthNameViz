c857da18f22fb3c809b854c754145752b13e7eb8
MonsieurMajy <mickael.adriao@gmail.com>
2023-06-16 11:46:42 +0200

    WIP first selector

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,44 +4,21 @@ import departementsJSON from '../assets/departments.json'
 import birthNamesCSV from '../assets/dpt2020.csv';
 import '../styles/index.scss';
 
-// load csv
-d3.csv(birthNamesCSV).then(function (data) {
-    console.log(data);
-});
+////////////Constants////////////
+const widthColumn1 = 975;
+const heightMap = 610;
+const heightSelectors = 200;
+const minYear = 1900; //TODO: Change that according to the data
+const maxYear = 2010; //TODO: Change that according to the data
 
+
+////////////Interfaces////////////
 // declare the type of the imported JSON
 interface GeoJSON {
     type: string;
     features: any[];
     bbox: number[];
 }
-
-// enforce type checking on the imported JSON
-const departements: GeoJSON = departementsJSON;
-
-console.log(departements);
-
-// most of this code comes from https://www.datavis.fr/d3js/map-firststep
-
-const width = 975;
-const height = 610;
-
-const path = d3.geoPath();
-
-const projection = d3.geoConicConformal()
-    .center([2.454071, 46.279229])
-    .scale(2600)
-    .translate([width / 2, height / 2]);
-
-path.projection(projection);
-
-const svg = d3.select('body').append("svg")
-    .attr("id", "svg")
-    .attr("width", width)
-    .attr("height", height);
-
-const deps = svg.append("g")
-    .attr("id", "departements");
 
 interface Departement {
     properties: {
@@ -58,6 +35,57 @@ interface Departement {
         Y_CHF_LIEU: number;
     }
 }
+
+
+////////////Loading data////////////
+// load csv
+d3.csv(birthNamesCSV).then(function (data) {
+    console.log(data);
+});
+
+////////////Seletor pannel////////////
+const selectors = d3.select('body').append("div")
+    .attr("class", "pannel")
+    .attr("id", "selectors")
+    .attr("width", widthColumn1)
+    .attr("height", heightSelectors);
+
+const periodSel = selectors.append("div");
+
+periodSel.append("input")
+    .attr("type", "range")
+    .attr("id", "period")
+    .attr("min", minYear)
+    .attr("max", maxYear);
+
+periodSel.append("label").html("Period");
+
+////////////Map pannel////////////
+
+// enforce type checking on the imported JSON
+const departements: GeoJSON = departementsJSON;
+
+console.log(departements);
+
+// most of this code comes from https://www.datavis.fr/d3js/map-firststep
+
+const path = d3.geoPath();
+
+const projection = d3.geoConicConformal()
+    .center([2.454071, 46.279229])
+    .scale(2600)
+    .translate([widthColumn1 / 2, heightMap / 2]);
+
+path.projection(projection);
+
+const map = d3.select('body').append("svg")
+    .attr("class", "pannel")
+    .attr("id", "map")
+    .attr("width", widthColumn1)
+    .attr("height", heightMap);
+
+const deps = map.append("g")
+    .attr("id", "departements");
 
 deps.selectAll("path")
     .data(departements.features)
